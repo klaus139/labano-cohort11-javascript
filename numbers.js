fb4729071173f9c265e560daf6d42173f88b2c5b
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2024-11-02 13:21:14 +0100

    added number manioulation

---
+++ numbers.js
@@ -0,0 +1,37 @@
+let x = 1
+
+let y = 3
+
+//add two numbers
+let z = x + y
+
+//modules %
+
+const a = 6
+
+const b = "4"
+
+const c = a / b
+
+
+//odd and even numbers
+const value = 89
+const answer = 91 % 2
+
+
+const myNumber = 22.8
+//round up this number
+
+const roundedNumber = Math.round(myNumber);//obeys maths
+
+const roundedUpNumber = Math.ceil(myNumber)// rounds it up to the highes number
+
+const roundedDownNumber = Math.floor(myNumber)
+
+console.log(roundedDownNumber)
+
+
+
+
+
+
